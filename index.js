836d5a7d6ecbc1a909d34304e52cda57564edfe0
AliHasan06 <alihassanraja06@gmail.com>
2024-12-07 04:00:07 +0500

    updated

--- index.js
+++ index.js
@@ -3,39 +3,22 @@ const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("ali");
-});
-app.get("/create", async (req, res) => {
-  let createdUser = await userModel.create({
-    name: "ali",
-    username: "alihasan06",
-    email: "alihassanraja06@gmail.com",
-  });
-  res.send(createdUser);
+  res.render("index");
 });
 
-app.get("/update", async (req, res) => {
-  let updatedUser = await userModel.findOneAndUpdate(
-    { username: "alihasan06" },
-    { name: "ali hasan" },
-    { new: true }
-  );
-  res.send(updatedUser);
+
+app.get("/read", (req, res) => {
+  res.render("read");
 });
 
-app.get("/read", async (req, res) => {
-  let users = await userModel.find();
 
-  res.send(users);
-});
-app.get("/delete", async (req, res) => {
-  let users = await userModel.findOneAndDelete({ username: "alihasan06" });
-
-  res.send(users);
-});
 
 app.listen(3000);

9a990462584e5394b580a61ea6de2e5a77666400
AliHasan06 <alihassanraja06@gmail.com>
2024-12-07 03:14:29 +0500

    updated

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-const userModel = require("./usermodel");
-
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.send("ali");
 });

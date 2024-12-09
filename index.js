fe5c3b5a46fa4ea7a21abfcebb03957742d22431
Alihasan06 <alihassanraja06@gmail.com>
2024-12-09 08:51:11 -0800

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const userModel = require("./models/user");
 
 app.set("view engine", "ejs");
 
@@ -19,6 +20,16 @@ app.get("/read", (req, res) => {
   res.render("read");
 });
 
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body
+ let createdUser = await userModel.create({
+    name,
+    email,
+    image,
+  })
+
+  res.send(createdUser)
+});
 
 
 app.listen(3000);
